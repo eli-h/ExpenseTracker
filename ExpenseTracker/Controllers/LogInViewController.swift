28825add6e6f1b1ddc70245381b700c3ed9dee0b
eli-h <eli.heineman@gmail.com>
2020-05-13 19:49:06 -0400

    removed initial sql code

--- ExpenseTracker/Controllers/LogInViewController.swift
+++ ExpenseTracker/Controllers/LogInViewController.swift
@@ -10,20 +10,9 @@ import UIKit
 import SQLite3
 
 class LogInViewController: UIViewController {
-    
-    var database: Connection!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        do {
-            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-            let fileUrl = documentDirectory.appendingPathComponent("users").appendingPathExtension("sqlite3")
-            let database = try Connection(fileUrl.path)
-            self.database = database
-        } catch {
-            print(error)
-        }
     }
     
 
